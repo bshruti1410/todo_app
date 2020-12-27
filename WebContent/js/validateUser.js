3d09bf599c4ada19c308eac65734e9b77efca834
Shruti Barnwal <shrutibarnwal99@gmail.com>
2020-12-27 10:23:30 +0530

    testing

--- WebContent/js/validateUser.js
+++ WebContent/js/validateUser.js
@@ -5,11 +5,9 @@ myApp.controller('validateUserController', function ($scope, $http) {
 	$scope.obj = {userName:'', password: ''};
 	
 	$scope.validateUser = function() {
-		
 		$http.get('/todo_app/LoginController').then(function(response) {
 			var res = response.data;
 			alert(hello);
 		});
-		
 	}
 });
